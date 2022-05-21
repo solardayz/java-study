dd1c261f66953f47c914e866018eef8e30473c9e
a-home <solardayz@naver.com>
2022-05-21 22:19:24 +0900

    전략 패턴 공부

---
+++ src/com/solardayz/pattern/strategy/Duck.java
@@ -0,0 +1,11 @@
+package com.solardayz.pattern.strategy;
+
+public class Duck {
+
+    void quick() {
+        System.out.println("꽥꽥");
+    }
+    void fly() {
+        System.out.println("난다");
+    }
+}
